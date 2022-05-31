90f277464f7d49149dea69f30af81c89a7afdac0
SANSKAR PANDEY <78846892+PANDEYSANSKAR@users.noreply.github.com>
2022-05-31 14:51:05 +0530

    Update readme.m

--- Neural Networks Basics/readme.m
+++ Neural Networks Basics/readme.m
@@ -1,1 +1,30 @@
 
+1)Python Basics with Numpy (optional assignment)
+Welcome to your first assignment. This exercise gives you a brief introduction to Python. Even if you've used Python before, this will help familiarize you with the functions we'll need.
+
+Instructions:
+
+You will be using Python 3.
+Avoid using for-loops and while-loops, unless you are explicitly told to do so.
+After coding your function, run the cell right below it to check if your result is correct.
+After this assignment you will:
+
+Be able to use iPython Notebooks
+Be able to use numpy functions and numpy matrix/vector operations
+Understand the concept of "broadcasting"
+Be able to vectorize code
+
+2) Logistic Regression with a Neural Network mindset
+Welcome to your first (required) programming assignment! You will build a logistic regression classifier to recognize cats. This assignment will step you through how to do this with a Neural Network mindset, and will also hone your intuitions about deep learning.
+
+Instructions:
+
+Do not use loops (for/while) in your code, unless the instructions explicitly ask you to do so.
+Use np.dot(X,Y) to calculate dot products.
+You will learn to:
+
+Build the general architecture of a learning algorithm, including:
+Initializing parameters
+Calculating the cost function and its gradient
+Using an optimization algorithm (gradient descent)
+Gather all three functions above into a main model function, in the right order.
