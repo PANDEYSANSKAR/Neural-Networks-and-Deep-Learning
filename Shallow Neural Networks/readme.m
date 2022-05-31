4c2406e73d4512012b87ed9c3280fc2db32c3c57
SANSKAR PANDEY <78846892+PANDEYSANSKAR@users.noreply.github.com>
2022-05-31 14:56:51 +0530

    Update readme.m

--- Shallow Neural Networks/readme.m
+++ Shallow Neural Networks/readme.m
@@ -1,1 +1,11 @@
 
+# Planar data classification with one hidden layer
+
+Welcome to your week 3 programming assignment! It's time to build your first neural network, which will have one hidden layer. Now, you'll notice a big difference between this model and the one you implemented previously using logistic regression.
+
+By the end of this assignment, you'll be able to:
+
+- Implement a 2-class classification neural network with a single hidden layer
+- Use units with a non-linear activation function, such as tanh
+- Compute the cross entropy loss
+- Implement forward and backward propagation
