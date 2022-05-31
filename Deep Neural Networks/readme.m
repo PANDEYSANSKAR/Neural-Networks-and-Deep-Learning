c9a9083505d4a38d1d5fa425563ce331a112969e
SANSKAR PANDEY <78846892+PANDEYSANSKAR@users.noreply.github.com>
2022-05-31 14:59:22 +0530

    Update readme.m

--- Deep Neural Networks/readme.m
+++ Deep Neural Networks/readme.m
@@ -1,1 +1,24 @@
 
+# Building your Deep Neural Network: Step by Step
+
+Welcome to your week 4 assignment (part 1 of 2)! Previously you trained a 2-layer Neural Network with a single hidden layer. This week, you will build a deep neural network with as many layers as you want!
+
+- In this notebook, you'll implement all the functions required to build a deep neural network.
+- For the next assignment, you'll use these functions to build a deep neural network for image classification.
+
+**By the end of this assignment, you'll be able to:**
+
+- Use non-linear units like ReLU to improve your model
+- Build a deeper neural network (with more than 1 hidden layer)
+- Implement an easy-to-use neural network class
+
+
+# Deep Neural Network for Image Classification: Application
+
+By the time you complete this notebook, you will have finished the last programming assignment of Week 4, and also the last programming assignment of Course 1! Go you! 
+
+To build your cat/not-a-cat classifier, you'll use the functions from the previous assignment to build a deep network. Hopefully, you'll see an improvement in accuracy over your previous logistic regression implementation.  
+
+**After this assignment you will be able to:**
+
+- Build and train a deep L-layer neural network, and apply it to supervised learning
